61681ff50ac6c16e6c081842cf20dc35caf8da19
Renan Ponick <ponickrenan@gmail.com>
2023-08-12 16:37:22 -0300

    feat: add post method and middleware json

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import { exercicio1 } from "./exercicios/exercicios-parte-um.js";
 // Cria uma instância do aplicativo Express.
 const app = express();
 
+// Middleware para permitir que o Express interprete o corpo da requisição como JSON
+app.use(express.json());
+
 app.get("/api/pessoa/:id", (req, res) => {
     const nome = req.query.nome;
     const pessoaId = req.params.id;
@@ -27,9 +30,17 @@ app.get("/api/exercicioum", (req, res) => {
     res.json({ message: `Resultado: ${result}` }).status(200);
 });
 
+app.post("/api/exercicio", (req, res) => {
+    console.log(req.body)
+    const num1 = req.body.num1;
+    const num2 = req.body.num2;
+    const result = exercicio1(num1, num2);
+    
+    res.status(201).json({ message: `Resultado: ${result}` });
+});
 
 // Inicia o servidor Express na porta 3000 e define uma função de retorno de chamada.
-app.listen(3000, () => {
+app.listen(3001, () => {
     // Exibe uma mensagem no console quando o servidor estiver ativo.
     console.log("Servidor rodando na porta 3000");
 });
