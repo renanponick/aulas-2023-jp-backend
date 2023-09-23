804ccd61e798eb9852e74a13e857f060a01a6eb7
Renan Ponick <ponickrenan@gmail.com>
2023-09-23 16:19:58 -0300

    done pessoa

--- src/controllers/pessoa.js
+++ src/controllers/pessoa.js
@@ -12,8 +12,7 @@ class ControllerExercicio {
         res.status(200).json(result);
       } catch (error) {
         console.log(error)
-        res.status(500).json({ message: error.message});
-        
+        res.status(500).json({ message: error.message}); 
       }
     }
 
